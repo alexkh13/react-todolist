cc3c87c90679a228b0727492e8ec83eaabad89e4
alex <alex.khmelnitsky@ca.com>
2017-06-07 16:46:58 +0300

    Implemented storage

--- server/index.js
+++ server/index.js
@@ -1,52 +1,103 @@
 const _ = require('underscore');
+const shortid = require('shortid');
 const express = require('express');
 const bodyParser = require('body-parser');
+const tasks = require('json-fs-store')(__dirname + '/tasks');
 const app = express();
 
 app.use(bodyParser.json({limit: '5mb'}));
 
-const tasks = [
-    { "id": 1, "name": "Task1", "status": "Open" },
-    { "id": 2, "name": "Task2", "status": "Closed" }
-];
-
-const images = {};
-
-let lastId = 2;
-
 app.get('/tasks', (req, res) => {
-    res.send(tasks);
+    tasks.list(function(err, tasks) {
+        if (err) {
+            res.status(500);
+            res.send(err);
+            return;
+        }
+        res.send(tasks);
+    });
 });
 
 app.post('/tasks', (req, res) => {
-    req.body.id = ++lastId;
-    tasks.push(req.body);
-    res.send(req.body);
+    req.body.id = shortid.generate();
+    tasks.add(req.body, function(err) {
+        if (err) {
+            res.status(500);
+            res.send(err);
+            return;
+        }
+        res.send(req.body);
+    });
+
 });
 
 app.put('/tasks', (req, res) => {
     let task = _.findWhere(tasks, { id: req.body.id });
-    _.extend(task, req.body);
-    res.send(task);
+
+    tasks.add(req.body, function(err) {
+        if (err) {
+            res.status(500);
+            res.send(err);
+            return;
+        }
+        _.extend(task, req.body);
+        res.send(task);
+    });
 });
 
-app.get('/tasks/:taskId/images', (req, res) => {
-    res.send(images[req.params.taskId] || []);
+app.delete('/tasks/:taskId', (req, res) => {
+    tasks.remove(req.params.taskId, function(err) {
+        if (err) {
+            res.status(500);
+            res.send(err);
+            return;
+        }
+        res.end();
+    });
 });
 
 app.post('/tasks/:taskId/images', (req, res) => {
-    if (!images[req.params.taskId]) {
-        images[req.params.taskId] = [];
-    }
-    images[req.params.taskId].push(req.body.data);
-    res.end();
+    tasks.load(req.params.taskId, function(err, task) {
+        if (err) {
+            res.status(500);
+            res.send(err);
+            return;
+        }
+        if (!task.images) {
+            task.images = [];
+        }
+        task.images.push(req.body.data);
+        tasks.add(task, function(err) {
+            if (err) {
+                res.status(500);
+                res.send(err);
+                return;
+            }
+            res.send(task);
+        });
+    });
 });
 
 app.delete('/tasks/:taskId/images/:index', (req, res) => {
-    if (images[req.params.taskId] && +req.params.index < images[req.params.taskId].length) {
-        images[req.params.taskId].splice(+req.params.index, 1);
-    }
-    res.send({});
+
+    tasks.load(req.params.taskId, function(err, task) {
+        if (err) {
+            res.status(500);
+            res.send(err);
+            return;
+        }
+        if (task.images && +req.params.index < task.images.length) {
+            task.images.splice(+req.params.index, 1);
+        }
+        tasks.add(task, function(err) {
+            if (err) {
+                res.status(500);
+                res.send(err);
+                return;
+            }
+            res.send(task);
+        });
+    });
 });
 
 app.listen(3001);
